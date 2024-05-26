91f6885c3ac0ee0904cb9745730953d64de99b92
Buddy362 <danila.malyshko00@gmail.com>
2024-05-26 18:57:32 +0500

    success

--- controllers/EspController.js
+++ controllers/EspController.js
@@ -5,7 +5,7 @@ export const EspPost = async (req, res) => {
     const { esp, mq2, mq5, humidity, temperature } = req.body;
     const newData = new EspModel({ esp, mq2, mq5, humidity, temperature });
     await newData.save();
-    res.status(201).send("Failed to save data");
+    res.status(201).send("Success");
   } catch (error) {
     res.status(400).send({ error: "Failed to save data" });
   }
